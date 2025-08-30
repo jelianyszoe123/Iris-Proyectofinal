4b8b66c57c47b46a8f2a9981dd458f4f47a6a351
jelianyszoe123 <jelianysortiz19713@gmail.com>
2025-08-30 09:56:55 -0400

    Update projectData.js
    Iris y Emprendimiento social

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -7,12 +7,12 @@ export const projects = [
       es: "Aventura",
     },
     description: {
-      en: "",
-      es: "",
+      en: "this is a page from an interactive book iris my story where you will be able to choose what happens",
+      es: "esta es una página de un libro interactivo iris mi historia en donde tú vas a poder elegir lo que pasa",
     },
     image: "",
-    liveSite: "",
-    githubRepo: "",
+    liveSite: "https://jelianyszoe123.github.io/Iris/",
+    githubRepo: "https://github.com/jelianyszoe123/Iris",
   },
     {
     id: 2,
@@ -21,11 +21,11 @@ export const projects = [
       es: "Emprendimiento",
     },
     description: {
-      en: "",
-      es: "",
+      en: "This is a page where I am explaining what I want to do when I grow up and what my social entrepreneurship will be about.",
+      es: "este esta es una página en donde estoy explicando lo que quiero hacer cuando sea grande y de qué se va a tratar mi emprendimiento social",
     },
     image: "",
-    liveSite: "",
-    githubRepo: "",
+    liveSite: " https://jelianyszoe123.github.io/name/",
+    githubRepo: "https://github.com/jelianyszoe123/name",
   },
 ];
